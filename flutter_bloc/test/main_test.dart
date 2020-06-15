7eb24559b536356582824b2f8454d54985850f47
Eddy <eduard.selenzow@smail.th-koeln.de>
2020-06-15 13:44:36 +0200

    added main_test file and weather_bloc tests

---
+++ flutter_bloc/test/main_test.dart
@@ -0,0 +1,11 @@
+import './blocs/weather_bloc_test.dart' as weather_bloc;
+
+void main() {
+  // Initialises the code generation for [get_it].
+
+  // bloc
+  weather_bloc.main();
+  // pages
+
+  // widgets
+}
