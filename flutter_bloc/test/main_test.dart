6056e18973837a0e347fa630f659cce99137ad45
Eddy <eduard.selenzow@smail.th-koeln.de>
2020-06-15 14:29:46 +0200

    updated main_test.dart and deleted duplicate file

--- flutter_bloc/test/main_test.dart
+++ flutter_bloc/test/main_test.dart
@@ -1,10 +1,22 @@
-import './blocs/weather_bloc_test.dart' as weather_bloc;
+import './blocs/settings/settings_bloc_test.dart' as settings_bloc;
+import './blocs/settings/settings_event_test.dart' as settings_event;
+import './blocs/settings/settings_state_test.dart' as settings_state;
+import './blocs/weather/weather_bloc_test.dart' as weather_bloc;
+import './blocs/weather/weather_event_test.dart' as weather_event;
+import './blocs/weather/weather_state_test.dart' as weather_state;
 
 void main() {
   // Initialises the code generation for [get_it].
 
   // bloc
   weather_bloc.main();
+  weather_event.main();
+  weather_state.main();
+
+  settings_bloc.main();
+  settings_event.main();
+  settings_state.main();
+
   // pages
 
   // widgets
